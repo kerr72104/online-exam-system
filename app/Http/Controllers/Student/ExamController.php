98448d5a94e7840050c086a5ab0b61be953ae637
MercxW <willandrewmercado@gmail.com>
2026-06-22 14:57:25 +0800

    Student Dashboard and Controllers
    updated the student dashboard, updated the Controllers for the students. (Minor adjustments needed)

--- app/Http/Controllers/Student/ExamController.php
+++ app/Http/Controllers/Student/ExamController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exam;
 use Illuminate\Http\Request;
 
 class ExamController extends Controller
 {
-    public function results(){
+    public function results()
+    {
         return view('student.results');
     }
 
-    public function show(Exam $exam){
+    public function show(Exam $exam)
+    {
         return view('student.exams.show', compact('exam'));
     }
-
 }
